e4e87ae9aee79e97cd2df0f1c8e761c22b68c05e
rajyadav-590 <rajyadavetah590@gmail.com>
2026-04-19 13:53:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 const fetch = require('node-fetch'); // Using node-fetch for API calls
 
+const cors = require('cors');
+
 const app = express();
+app.use(cors()); 
 const PORT = process.env.PORT || 3000;
 
 // Middleware
